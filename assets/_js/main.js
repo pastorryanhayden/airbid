1a910a9990f203d85cdb4d31dfd4e1344016ee74
carlosanaya01 <carlosanaya01@yahoo.es>
2017-09-29 16:23:17 -0300

    Main markup

---
+++ assets/_js/main.js
@@ -0,0 +1,16 @@
+// var contentOffset = document.getElementById("content").offsetTop;
+// var navigation = document.getElementById("navigation");
+// var navigationFixed = false
+
+// window.addEventListener("scroll", function(e){
+// 	var pageOffset = window.pageYOffset;
+// 	if(pageOffset >= contentOffset && !navigationFixed){
+// 		if(!navigationFixed){
+// 			navigation.classList.toggle("fixed");
+// 		}
+// 		navigationFixed = true;
+// 	}else if(navigationFixed){
+// 		navigationFixed = false;
+// 	}
+// });
+
